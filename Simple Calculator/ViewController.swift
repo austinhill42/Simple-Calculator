c37098d365b53cf7d9aafb9238294a79148fc6b1
Austin Hill <austin.hill42@gmail.com>
2018-01-27 22:04:56 -0600

    added outlets and action prevented the status bar from being overridden by the view controller background finalized the main storyboard

--- Simple Calculator/ViewController.swift
+++ Simple Calculator/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btn_ac: UIButton!
+    @IBOutlet weak var btn_posneg: UIButton!
+    @IBOutlet weak var btn_percent: UIButton!
+    @IBOutlet weak var btn_div: UIButton!
+    @IBOutlet weak var btn_mul: UIButton!
+    @IBOutlet weak var btn_sub: UIButton!
+    @IBOutlet weak var btn_add: UIButton!
+    @IBOutlet weak var btn_equal: UIButton!
+    @IBOutlet weak var btn_0: UIButton!
+    @IBOutlet weak var btn_1: UIButton!
+    @IBOutlet weak var btn_2: UIButton!
+    @IBOutlet weak var btn_3: UIButton!
+    @IBOutlet weak var btn_4: UIButton!
+    @IBOutlet weak var btn_5: UIButton!
+    @IBOutlet weak var btn_6: UIButton!
+    @IBOutlet weak var btn_7: UIButton!
+    @IBOutlet weak var btn_8: UIButton!
+    @IBOutlet weak var btn_9: UIButton!
+    @IBOutlet weak var btn_period: UIButton!
+    
+    @IBAction func buttonPressed(_ Sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +42,20 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // prevent the statusbar from being washed out by the view background
+        UIApplication.shared.statusBarStyle = .lightContent
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // reset the status bar to the default
+        UIApplication.shared.statusBarStyle = UIStatusBarStyle.default
     }
 
 
